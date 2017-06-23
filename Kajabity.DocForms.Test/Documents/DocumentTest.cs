619ddf8585dc68a7e82de64df9879cc633fbc25d
Simon Williams <Simon@jungle.local>
2017-06-23 22:06:31 +0100

    Added initial unit tests.
    Also, Visual Studio 2017 update.

---
+++ Kajabity.DocForms.Test/Documents/DocumentTest.cs
@@ -0,0 +1,200 @@
+﻿/*
+ * Copyright 2009-17 Williams Technologies Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Kajbity is a trademark of Williams Technologies Limited.
+ *
+ * http://www.kajabity.com
+ */
+
+using Kajabity.DocForms.Documents;
+using NUnit.Framework;
+using System;
+
+namespace Kajabity.DocForms.Test.Documents
+{
+    [TestFixture]
+    public class DocumentTest
+    {
+        [Test]
+        public void TestDocumentConstructionWithoutName()
+        {
+            Document underTest = new TestableDocument();
+            Assert.AreEqual(null, underTest.Name);
+            Assert.AreEqual(false, underTest.Modified);
+        }
+
+        [Test]
+        public void TestDocumentConstructionWithName()
+        {
+            const String name = "test document name";
+            Document underTest = new TestableDocument(name);
+            Assert.AreEqual(name, underTest.Name );
+            Assert.AreEqual(false, underTest.Modified);
+        }
+
+        [Test]
+        public void TestDocumentSetNameEvent()
+        {
+            const String name = "test document name";
+            Document underTest = new TestableDocument();
+
+            bool called = false;
+            underTest.StatusChanged += delegate (object sender, EventArgs e)
+            {
+                called = true;
+            };
+
+            underTest.Name = name;
+
+            Assert.AreEqual(name, underTest.Name);
+            Assert.AreEqual(true, called);
+
+            // Changing the name doesn't count as changing the document.
+            Assert.AreEqual(false, underTest.Modified);
+        }
+
+        [Test]
+        public void TestDocumentChangeNameEvent()
+        {
+            const String name = "test document name";
+            Document underTest = new TestableDocument("Original name");
+
+            bool called = false;
+            underTest.StatusChanged += delegate (object sender, EventArgs e)
+            {
+                called = true;
+            };
+
+            underTest.Name = name;
+
+            Assert.AreEqual(name, underTest.Name);
+            Assert.AreEqual(true, called);
+
+            // Changing the name doesn't count as changing the document.
+            Assert.AreEqual(false, underTest.Modified);
+        }
+
+        [Test]
+        public void TestDocumentSetSameNameEvent()
+        {
+            const String name = null;
+            Document underTest = new TestableDocument();
+
+            bool called = false;
+            underTest.StatusChanged += delegate (object sender, EventArgs e)
+            {
+                called = true;
+            };
+
+            underTest.Name = name;
+
+            Assert.AreEqual(name, underTest.Name);
+            Assert.AreEqual(false, called);
+
+            // Changing the name doesn't count as changing the document.
+            Assert.AreEqual(false, underTest.Modified);
+        }
+
+        [Test]
+        public void TestDocumentChangeSameNameEvent()
+        {
+            const String name = "test document name";
+            Document underTest = new TestableDocument(name);
+
+            bool called = false;
+            underTest.StatusChanged += delegate (object sender, EventArgs e)
+            {
+                called = true;
+            };
+
+            underTest.Name = name;
+
+            Assert.AreEqual(name, underTest.Name);
+            Assert.AreEqual(false, called);
+
+            // Changing the name doesn't count as changing the document.
+            Assert.AreEqual(false, underTest.Modified);
+        }
+
+        [Test]
+        public void TestDocumentChangeModifiedEvent()
+        {
+            bool newValue = true;
+            Document underTest = new TestableDocument();
+
+            bool called = false;
+            underTest.StatusChanged += delegate (object sender, EventArgs e)
+            {
+                called = true;
+            };
+
+            underTest.Modified = newValue;
+
+            Assert.AreEqual(newValue, underTest.Modified);
+            Assert.AreEqual(true, called);
+
+            // Now set it back again.
+
+            newValue = false;
+            called = false;
+
+            underTest.Modified = newValue;
+
+            Assert.AreEqual(newValue, underTest.Modified);
+            Assert.AreEqual(true, called);
+        }
+
+        [Test]
+        public void TestDocumentSameModifiedNoEvent()
+        {
+            bool newValue = false;
+            Document underTest = new TestableDocument();
+
+            bool called = false;
+            underTest.StatusChanged += delegate (object sender, EventArgs e)
+            {
+                called = true;
+            };
+
+            underTest.Modified = newValue;
+
+            Assert.AreEqual(newValue, underTest.Modified);
+            Assert.AreEqual(false, called);
+
+            // Now try true to true.
+
+            newValue = true;
+            underTest.Modified = newValue; // Ignored.
+
+            called = false;
+
+            underTest.Modified = newValue;
+
+            Assert.AreEqual(newValue, underTest.Modified);
+            Assert.AreEqual(false, called);
+        }
+
+        public class TestableDocument : Document
+        {
+            public TestableDocument()
+            {
+            }
+
+            public TestableDocument(string name) : base(name)
+            {
+            }
+        }
+    }
+}
---
+++ Kajabity.DocForms.Test/WindowsHelperTest.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Copyright 2009-17 Williams Technologies Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Kajbity is a trademark of Williams Technologies Limited.
+ *
+ * http://www.kajabity.com
+ */
+
+using NUnit.Framework;
+
+namespace Kajabity.DocForms.Test
+{
+    [TestFixture]
+    public class WindowsHelperTest
+    {
+        [Test]
+        public void TestGetShortPathShortStrings()
+        {
+            const int length = 50;
+            string[] short_strings =
+            {
+                null,
+                "",
+                "a",
+                "abcdefg",
+                "temp.txt",
+                "D:\\.gitignore",
+                "D:\\appveyor.yml",
+                "D:\\Kajabity.DocForms\\bin",
+                "D:\\Kajabity.DocForms\\Documents",
+                "D:\\Kajabity.DocForms\\Forms",
+                "D:\\Kajabity.DocForms\\Kajabity.DocForms.csproj"
+            };
+
+            foreach (string s in short_strings)
+            {
+                string result = WindowsHelper.GetShortPath(s, length);
+
+                Assert.AreEqual(s, result);
+            }
+        }
+
+        [Test]
+        public void TestGetShortPathLongStrings()
+        {
+            const int length = 50;
+            string[] short_strings =
+            {
+                "D:\\Kajabity.DocForms\\Kajabity.DocForms.csproj.use",
+                "D:\\Kajabity.DocForms\\Kajabity.DocForms.csproj.user",
+                "D:\\Kajabity.DocForms\\obj\\Debug\\Kajabity.DocForms.csprojResolveAssemblyReference.cache",
+                "D:\\Kajabity.DocForms.Test\\obj\\Debug\\TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs",
+                "D:\\packages\\Microsoft.NETCore.Portable.Compatibility.1.0.1\\lib\\netstandard1.0\\System.ComponentModel.DataAnnotations.dll",
+                "D:\\packages\\System.Runtime.InteropServices.RuntimeInformation.4.0.0\\runtimes\\win\\lib\\netstandard1.1\\System.Runtime.InteropServices.RuntimeInformation.dll"
+            };
+
+            foreach (string s in short_strings)
+            {
+                string result = WindowsHelper.GetShortPath(s, length);
+
+                Assert.AreEqual(length-1, result.Length);
+            }
+        }
+    }
+}
