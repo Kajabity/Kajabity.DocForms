a78ab6158027a113d75c929a04cb3309a9eccb05
Simon Williams <Simon@jungle.local>
2017-06-24 00:24:37 +0100

    Changed NewFile property to false if no Document.

--- Kajabity.DocForms/Documents/SingleDocumentManager.cs
+++ Kajabity.DocForms/Documents/SingleDocumentManager.cs
@@ -34,6 +34,11 @@ namespace Kajabity.DocForms.Documents
     public abstract class SingleDocumentManager<TDocument> where TDocument : Document
     {
         /// <summary>
+        /// The default document name if none is provided by sub-classes.
+        /// </summary>
+        public const string DEFAULT_DOCUMENT_NAME = "Document";
+
+        /// <summary>
         /// The filename extension (without the dot) for the type of files managed by this class.
         /// </summary>
         public string DefaultExtension { get; set; }
@@ -41,17 +46,25 @@ namespace Kajabity.DocForms.Documents
         /// <summary>
         /// The default name or filename (without path or extension) for a file created by this class.
         /// </summary>
-        public string DefaultName { get; set; } = "Document";
+        public string DefaultName { get; set; } = DEFAULT_DOCUMENT_NAME;
 
         /// <summary>
         /// The filename of the current document.  Will be null of no current document.
         /// </summary>
         public string Filename { get; private set; }
 
+        private bool _newFile = false;
+
         /// <summary>
         ///	Returns true if the current document (if any) is new and unsaved.
         /// </summary>
-        public bool NewFile { get; private set; } = true;
+        public bool NewFile
+        {
+            get
+            {
+                return Opened && _newFile;
+            }
+        }
 
         /// <summary>
         /// Counts the number of documents openned by this instance of the Document Manager to 
@@ -68,14 +81,14 @@ namespace Kajabity.DocForms.Documents
             get { return _document; }
             protected set
             {
-                if( _document != null )
+                if (_document != null)
                 {
                     _document.StatusChanged -= Document_OnStatusChanged;
                 }
 
                 _document = value;
 
-                if( _document != null )
+                if (_document != null)
                 {
                     _document.StatusChanged += Document_OnStatusChanged;
                 }
@@ -87,9 +100,9 @@ namespace Kajabity.DocForms.Documents
         /// </summary>
         /// <param name="sender">unused.</param>
         /// <param name="eventArgs">event arguments from the document passed through.</param>
-        private void Document_OnStatusChanged( object sender, EventArgs eventArgs )
+        private void Document_OnStatusChanged(object sender, EventArgs eventArgs)
         {
-            OnDocumentStatusChanged( eventArgs );
+            OnDocumentStatusChanged(eventArgs);
         }
 
         /// <summary>
@@ -120,9 +133,9 @@ namespace Kajabity.DocForms.Documents
         {
             string documentName = string.Format(DefaultName + "{0}." + DefaultExtension, ++_docCount);
             Filename = documentName;
-            NewFile = true;
+            _newFile = true;
 
-            if( Document != null )
+            if (Document != null)
             {
                 Document.Name = documentName;
                 Document.Modified = false;
@@ -135,19 +148,19 @@ namespace Kajabity.DocForms.Documents
         /// once document set (<code>base.Load(filename);</code>).
         /// </summary>
         /// <param name="filename">filename and path loaded</param>
-        public virtual void Load( string filename )
+        public virtual void Load(string filename)
         {
             Filename = filename;
-            NewFile = false;
+            _newFile = false;
 
-            if( Document != null )
+            if (Document != null)
             {
                 FileInfo fileInfo = new FileInfo(filename);
                 Document.Name = fileInfo.Name;
                 Document.Modified = false;
             }
 
-            OnDocumentStatusChanged( new EventArgs() );
+            OnDocumentStatusChanged(new EventArgs());
         }
 
         /// <summary>
@@ -156,12 +169,12 @@ namespace Kajabity.DocForms.Documents
         /// (<code>base.Save(filename);</code>).
         /// </summary>
         /// <param name="filename">filename and path to save to</param>
-        public virtual void Save( string filename )
+        public virtual void Save(string filename)
         {
             Filename = filename;
-            NewFile = false;
+            _newFile = false;
 
-            if( Document != null )
+            if (Document != null)
             {
                 FileInfo fileInfo = new FileInfo(filename);
                 Document.Name = fileInfo.Name;
@@ -178,7 +191,7 @@ namespace Kajabity.DocForms.Documents
         public virtual void Close()
         {
             Document = null;
-            NewFile = false;
+            _newFile = false;
             Filename = null;
         }
 
@@ -189,17 +202,17 @@ namespace Kajabity.DocForms.Documents
         /// update window title with filename or status display.
         /// </summary>
         [
-        Category( "Document" ),
-        Description( "Notifies whenever a document's name or modified status are changed." )
+        Category("Document"),
+        Description("Notifies whenever a document's name or modified status are changed.")
         ]
         public event EventHandler<EventArgs> DocumentStatusChanged;
 
         /// <summary>
         /// Called whenever a document name or modified status is or may have been changed.
         /// </summary>
-        protected virtual void OnDocumentStatusChanged( EventArgs args )
+        protected virtual void OnDocumentStatusChanged(EventArgs args)
         {
-            DocumentStatusChanged?.Invoke( this, args );
+            DocumentStatusChanged?.Invoke(this, args);
         }
 
     }
